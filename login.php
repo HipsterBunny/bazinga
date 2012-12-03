d8a4a20ec2d0884a5882882e70eaf123d8b747f3
Dylan Swartz <mail@dylanswartz.com>
2012-12-02 23:00:35 -0500

    registration working

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php 
+include("overall_header.php");
+?>
+
+<div class="span9">
+	<form class="form-horizontal" action='' method="POST">
+	  <fieldset>
+	    <div id="legend">
+	      <legend class="">Login</legend>
+	    </div>
+	    <div class="control-group">
+	      <!-- Username -->
+	      <label class="control-label"  for="email">Email</label>
+	      <div class="controls">
+	        <input type="text" id="email" name="email" placeholder="jen@bazingabooks.com" class="input-xlarge">
+	      </div>
+	    </div>
+
+	    <div class="control-group">
+	      <!-- Password-->
+	      <label class="control-label" for="password">Password</label>
+	      <div class="controls">
+	        <input type="password" id="password" name="password" placeholder="************" class="input-xlarge">
+	      </div>
+	    </div>
+
+
+	    <div class="control-group">
+	      <!-- Button -->
+	      <div class="controls">
+	        <button class="btn btn-primary">Login</button>
+	      </div>
+	    </div>
+	  </fieldset>
+	</form>
+</div>
+
+<?php
+include("overall_footer.php"); 
+?>
--- signup.php
+++ signup.php
@@ -1,25 +1,104 @@
 <?php 
-include("overall_header.php");
-?>
-
-<div class="span9">
+require("config.php");
+$p = $_GET['p'];
+$form = '
     <h2>Sign Up</h2>
-    <form>
+    <form action="signup.php?p=newuser" method="post">
     <label>First Name</label>
     <input type="text" name="firstname" class="span3">
     <label>Last Name</label>
     <input type="text" name="lastname" class="span3">
     <label>Email Address</label>
     <input type="email" name="email" class="span3">
-    <label>Username</label>
-    <input type="text" name="username" class="span3">
     <label>Password</label>
     <input type="password" name="password" class="span3">
     <label><input type="checkbox" name="terms"> I agree with the <a data-toggle="modal" href="#termModal">Terms and Conditions</a>.</label>
     <input type="submit" value="Sign up" class="btn btn-primary pull-right">
     <div class="clearfix"></div>
-    </form>
-</div>
+    </form>';
+
+
+if ($p =='newuser') {
+		$fname = trim($_POST['firstname']);
+		$lname = trim($_POST['lastname']);
+		$email = trim($_POST['email']);
+		$pass  = trim($_POST['password']);
+		$terms = trim($_POST['terms']);
+		
+		$error = false;
+		$success = false;
+		$errors = array();
+		
+		if (!isset($fname) || strlen($fname) < 3) {
+			$error = true;
+			array_push($errors, "Invalid first name!");
+		}
+		if (!isset($lname) || strlen($lname) < 3) {
+			$error = true;
+			array_push($errors, "Invalid last name!");
+		}
+		if (!isset($email) || strlen($email) < 4) {
+			$error = true;
+			array_push($errors, "Invalid email address!");
+		}
+		if (!isset($pass) || strlen($pass) < 4) {
+			$error = true;
+			array_push($errors, "Your password sux.");
+		}
+		if (!terms) {
+			$error = true;
+			array_push($errors, "You can't signup without agreeing to the terms.");
+		}
+		
+		if (!$error) {
+		
+			try {
+			    $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
+			    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+				$sql = "INSERT INTO users (fname, lname, email, password) VALUES(:fname, :lname, :email, :pass)";
+				$qry = $conn->prepare($sql);
+			    $qry->execute(array(':fname'=>$fname,
+				                    ':lname'=>$lname,
+								    ':email'=>$email,
+							    	':pass'=>md5(pass)));
+				
+
+				// $res = $qry->fetchAll();
+
+				$success=true;
+
+			} catch(PDOException $e) {
+			    $error = true;
+				array_push($errors, $e);
+			}
+			
+		}
+		
+		// if (!$error && !$success) {
+		// 	$error = true;
+		// 	array_push($errors, "An unknown error occured.");
+		// }
+		if ($success) {
+			header( "refresh:6;url=login.php?p=newuser" );
+		}
+}
+
+
+include("overall_header.php");
+echo "<div class=\"span9\">";
+if ($error) {
+	echo "<div class='alert alert-error'>".$errors[0]."</div>";
+}
+if ($success) {
+	echo "<div class='alert alert-success'>Hurray! You made an account!</div>";
+	echo '<p>Your browser should redirect you in 5 seconds... ' .
+		 'If not, click the button below! <br/><br/><a class="btn btn-large btn-primary" href="login.php?p=newuser" >Login</a></p>';
+} else {
+	echo $form;
+}
+echo "</div>";
+?>
 
 <div id="termModal" class="modal hide fade">
   <div class="modal-header">
@@ -35,5 +114,7 @@ include("overall_header.php");
 </div>
 
 <?php 
+
 include("overall_footer.php");
+
 ?>
